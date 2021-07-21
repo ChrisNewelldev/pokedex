3a5528dffeb4e665685a6461e7650fe3e87a2f89
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-21 16:19:42 -0600

    need to fix capitalization on My vs mypokemon

--- app/Models/Pokemon.js
+++ app/Models/Pokemon.js
@@ -15,13 +15,25 @@ export default class Pokemon {
 get Template() {
   return `
   <div class="bg-light m3 p-3 shadow">
-  <div>
-  <h4 class='uppercase'>${this.name}</h4>
-  
-</div>
-<div class="text-right">
- ${this.Button}
-</div>
-</div>
+    <div>
+      <h4 class='uppercase'>${this.name}</h4>
+      <img>Img: ${this.img}</img>
+      <p>Weight: ${this.weight}</p>
+      <p>Height: ${this.height}</p>
+      <p>Type: ${this.type}</p>
+      <p>Id: ${this.id}</p>
+    </div>
+    <div class="text-right">
+      ${this.Button}
+    </div>
+  </div>
   `
+}
+
+get Button() {
+  const exists = ProxyState.myPokemon.find(p => p.name === ProxyState.activePokemon.name)
+
+  
+
+
 }
